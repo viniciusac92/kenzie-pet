9c47444baa67239047b8caf607c0ce6c6c509446
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-07-28 17:22:40 -0300

    🎨 feat: add fk to Animal model

--- pet_shop/models.py
+++ pet_shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class Group(models.Model):
@@ -11,6 +12,7 @@ class Animal(models.Model):
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 
 class Characteristic(models.Model):
