bb38d35b1c4c44ab349a8d8bd6b913c032752e32
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-07-28 16:45:00 -0300

    🍱 feat: add Animal model

--- pet_shop/models.py
+++ pet_shop/models.py
@@ -9,3 +9,5 @@ class Group(models.Model):
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     age = models.FloatField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=255)
