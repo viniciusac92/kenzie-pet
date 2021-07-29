3a56d8303d5f98072a2c002aac7d65fa4a605650
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-07-29 16:34:19 -0300

    ✅ feat: add Chars serializer

--- pet_shop/serializers.py
+++ pet_shop/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.fields import FloatField
+
+from pet_shop.models import Group
 
 
 class GroupSerializer(serializers.Serializer):
@@ -7,7 +10,16 @@ class GroupSerializer(serializers.Serializer):
     scientific_name = serializers.CharField()
 
 
+class AnimaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    age = serializers.FloatField()
+    weight = serializers.FloatField()
+    sex = serializers.CharField()
+    group = GroupSerializer()
+
+
 class CharacteristicSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    # animal_list = serializers.
+    animal_list = AnimaSerializer(many=True, read_only=True, source='animal_list')
